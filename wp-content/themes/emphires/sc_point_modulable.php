b20c8c249955d086750d5a600a318adef2d5f6a9
abdoulahadi <caambdiop@gmail.com>
2021-10-28 20:48:59 +0000

    rectification sur les points modulables

--- wp-content/themes/emphires/sc_point_modulable.php
+++ wp-content/themes/emphires/sc_point_modulable.php
@@ -16,37 +16,37 @@ if(!is_page( 'connexion' ) &&  !$_SESSION['mail']  ){
 $con = mysqli_connect("localhost","root","","ussein_candidature");
 $req1 = mysqli_query($con,"SELECT note FROM note_age");
 $tab1 = mysqli_fetch_all($req1);
-$can_note_age_1 = $tab1[0][0];
-$can_note_age_2 = $tab1[1][0];
-$can_note_age_3 = $tab1[2][0];
-$can_note_age_4 = $tab1[3][0];
-$can_note_age_5 = $tab1[4][0];
-$can_note_age_6 = $tab1[5][0];
-$can_note_age_7 = $tab1[6][0];
-$can_note_age_8 = $tab1[7][0];
-$can_note_age_9 = $tab1[8][0];
-$can_note_age_10 = $tab1[9][0];
-$can_note_age_11 = $tab1[10][0];
-$can_note_age_12 = $tab1[11][0];
+$can_note_age_1 = $tab1[6][0];
+$can_note_age_2 = $tab1[7][0];
+$can_note_age_3 = $tab1[8][0];
+$can_note_age_4 = $tab1[9][0];
+$can_note_age_5 = $tab1[10][0];
+$can_note_age_6 = $tab1[11][0];
+$can_note_age_7 = $tab1[0][0];
+$can_note_age_8 = $tab1[1][0];
+$can_note_age_9 = $tab1[2][0];
+$can_note_age_10 = $tab1[3][0];
+$can_note_age_11 = $tab1[4][0];
+$can_note_age_12 = $tab1[5][0];
 
 $req2 = mysqli_query($con,"SELECT note FROM ec_note_diplome");
 $tab2 = mysqli_fetch_all($req2);
 $can_note_diplome_1 = $tab2[0][0];
-$can_note_diplome_2 = $tab2[6][0]; //diplome de point 30, à faire attetion puisque c'est deernier sur la liste dans la base de données
-$can_note_diplome_3 = $tab2[1][0];
-$can_note_diplome_4 = $tab2[2][0];
-$can_note_diplome_5 = $tab2[3][0];
-$can_note_diplome_6 = $tab2[4][0];
-$can_note_diplome_7 = $tab2[5][0];
+$can_note_diplome_2 = $tab2[1][0]; //diplome de point 30, à faire attetion puisque c'est deernier sur la liste dans la base de données
+$can_note_diplome_3 = $tab2[2][0];
+$can_note_diplome_4 = $tab2[3][0];
+$can_note_diplome_5 = $tab2[4][0];
+$can_note_diplome_6 = $tab2[5][0];
+$can_note_diplome_7 = $tab2[6][0];
 
 $req3 = mysqli_query($con,"SELECT note FROM ec_note_licence_master");
 $tab3 = mysqli_fetch_all($req3);
-$can_note_licence_master_1 = $tab3[0][0];
-$can_note_licence_master_2 = $tab3[1][0];
-$can_note_licence_master_3 = $tab3[2][0];
-$can_note_licence_master_4 = $tab3[3][0];
-$can_note_licence_master_5 = $tab3[4][0];
-$can_note_licence_master_6 = $tab3[5][0];
+$can_note_licence_master_1 = $tab3[3][0];
+$can_note_licence_master_2 = $tab3[2][0];
+$can_note_licence_master_3 = $tab3[5][0];
+$can_note_licence_master_4 = $tab3[4][0];
+$can_note_licence_master_5 = $tab3[1][0];
+$can_note_licence_master_6 = $tab3[0][0];
 
 
 $req4 = mysqli_query($con,"SELECT note FROM ec_note_doctorat");
@@ -77,24 +77,24 @@ $can_note_experience_recherche_4 = $tab6[3][0];
 
 $req7 = mysqli_query($con,"SELECT note FROM ec_note_autre_experience");
 $tab7= mysqli_fetch_all($req7);
-$can_note_autre_experience_1 = $tab7[0][0];
-$can_note_autre_experience_2 = $tab7[1][0];
-$can_note_autre_experience_3 = $tab7[2][0];
-$can_note_autre_experience_4 = $tab7[3][0];
-$can_note_autre_experience_5 = $tab7[4][0];
+$can_note_autre_experience_1 = $tab7[2][0];
+$can_note_autre_experience_2 = $tab7[4][0];
+$can_note_autre_experience_3 = $tab7[3][0];
+$can_note_autre_experience_4 = $tab7[1][0];
+$can_note_autre_experience_5 = $tab7[0][0];
 
 $req15 = mysqli_query($con,"SELECT note FROM ec_note_grade");
 $tab15 = mysqli_fetch_all($req15);
-$can_note_grade_1 = $tab15[0][0];
-$can_note_grade_2 = $tab15[1][0];
-$can_note_grade_3 = $tab15[2][0];
-$can_note_grade_4 = $tab15[3][0];
+$can_note_grade_1 = $tab15[3][0];
+$can_note_grade_2 = $tab15[0][0];
+$can_note_grade_3 = $tab15[1][0];
+$can_note_grade_4 = $tab15[2][0];
 $can_note_grade_5 = $tab15[4][0];
 
 $req16 = mysqli_query($con,"SELECT note FROM ec_note_brevet");
 $tab16 = mysqli_fetch_all($req16);
-$can_note_brevet_1 = $tab16[0][0];
-$can_note_brevet_2 = $tab16[1][0];
+$can_note_brevet_1 = $tab16[1][0];
+$can_note_brevet_2 = $tab16[0][0];
 
 $req9 = mysqli_query($con,"SELECT note FROM ec_note_aid");
 $tab9= mysqli_fetch_all($req9);
@@ -131,17 +131,17 @@ $can_note_distinction_2 = $tab18[1][0];
 
 $req13 = mysqli_query($con,"SELECT note FROM ec_note_proccedings");
 $tab13= mysqli_fetch_all($req13);
-$can_note_proccedings_1 = $tab13[0][0];
-$can_note_proccedings_2 = $tab13[1][0];
+$can_note_proccedings_1 = $tab13[1][0];
+$can_note_proccedings_2 = $tab13[0][0];
 
 $req14 = mysqli_query($con,"SELECT note FROM ec_note_communication_conference");
 $tab14 = mysqli_fetch_all($req14);
-$can_note_communication_conference_1 = $tab14[0][0];
-$can_note_communication_conference_2 = $tab14[1][0];
+$can_note_communication_conference_1 = $tab14[1][0];
+$can_note_communication_conference_2 = $tab14[5][0];
 $can_note_communication_conference_3 = $tab14[2][0];
-$can_note_communication_conference_4 = $tab14[3][0];
-$can_note_communication_conference_5 = $tab14[4][0];
-$can_note_communication_conference_6 = $tab14[5][0];
+$can_note_communication_conference_4 = $tab14[0][0];
+$can_note_communication_conference_5 = $tab14[3][0];
+$can_note_communication_conference_6 = $tab14[4][0];
    
        //requete pour admin simple
 $mail=$_SESSION['mail'];
@@ -159,7 +159,7 @@ $tab=mysqli_fetch_array($requete);
     }
     body{
         
-        background-image: url('http://localhost/candidature/wp-content/uploads/2021/10/background.jpg');
+        background-image: url('http://localhost/candidature/wp-content/uploads/2021/09/background-scaled.jpg');
         background-position: 0;
         background-repeat: no-repeat;
         background-size: cover;
@@ -728,12 +728,12 @@ div.div_reinitialiser:hover{
 
 <summary> PUBLICATION</summary>
 <table>
-<tr>  <th rowspan="4">Article indéxé du domaine :</th> <td>auteur1</td>   <td><input type="text" name="aid_a1" value="<?php echo $can_note_aihd_2 ?>"> </td></tr>
+<tr>  <th rowspan="4">Article indéxé du domaine :</th> <td>auteur1</td>   <td><input type="text" name="aid_a1" value="<?php echo $can_note_aid_1 ?>"> </td></tr>
 <tr>  <td>auteur2</td> <td><input type="text" name="aid_a2" value="<?php echo $can_note_aid_2 ?>"> </td></tr>
 <tr>  <td>auteur3</td> <td><input type="text" name="aid_a3" value="<?php echo $can_note_aid_3 ?>"> </td></tr>
 <tr>  <td>autre auteur</td> <td><input type="text" name="aid_aa" value="<?php echo $can_note_aid_4 ?>"> </td></tr>
 
-<tr>  <th rowspan="4">Article indexé hors domaine, article non indexé du domaine et article de vulgarisation :</th> <td>auteur1</td>   <td><input type="text" name="aihd_a1" value="<?php echo $can_note_aihd_2 ?>"> </td></tr>
+<tr>  <th rowspan="4">Article indexé hors domaine, article non indexé du domaine et article de vulgarisation :</th> <td>auteur1</td>   <td><input type="text" name="aihd_a1" value="<?php echo $can_note_aihd_1 ?>"> </td></tr>
 <tr>  <td>auteur2</td> <td><input type="text" name="aihd_a2" value="<?php echo $can_note_aihd_2 ?>"> </td> </tr>
 <tr>  <td>auteur3</td> <td><input type="text" name="aihd_a3" value="<?php echo $can_note_aihd_3 ?>"> </td> </tr>
 <tr>  <td>autre auteur</td> <td><input type="text" name="aihd_aa" value="<?php echo $can_note_aihd_4 ?>"> </td></tr>
